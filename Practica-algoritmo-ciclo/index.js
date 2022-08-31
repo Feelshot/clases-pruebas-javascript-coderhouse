81013071d3de2dff2891214aa40c9b1c63799ea5
Feelshot <feelshot.marketing@gmail.com>
2022-08-31 16:49:48 -0500

    algoritmo con for

--- Practica-algoritmo-ciclo/index.js
+++ Practica-algoritmo-ciclo/index.js
@@ -4,3 +4,13 @@ while (contador < 11) {
     console.log ("segundo " + contador);
     contador ++;
 }
+
+
+/*
+for (inicializacion de la variable; condicion; decremento o incremento) {
+    sentencias a ejecutar el for
+}
+*/
+for (let minuto = 0; minuto < 11; minuto++) {
+    console.log ("minuto " +minuto);
+}
