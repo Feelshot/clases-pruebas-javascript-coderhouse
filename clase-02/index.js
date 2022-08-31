6a74f18bcfa73d66ec535f3d3ded6f7a0e2bed23
Feelshot <feelshot.marketing@gmail.com>
2022-08-31 18:48:36 -0500

    add 2da parte

--- clase-02/index.js
+++ clase-02/index.js
@@ -44,7 +44,55 @@ if ((nombre != "") && (nombre === "andres" || nombre === "ANDRES")) {
     alert("Nombre no identificado");
 }
 */
+
+/*
 let nombre = "camila";
+let edad = 5;
 
 console.log ("Hola " + nombre);
-console.log (`Hola ${nombre}`);
+console.log (`Como estas? ${nombre}`);
+console.log(`Hola ${nombre} tienes ${edad}`);
+*/
+
+
+//  PARTE 2 ---------------------
+
+/* Ciclos e iteraciones */
+
+/*
+for (let i = 0; 1 < 10; i++){
+    console.log("Hola");
+}
+
+for (let i = 10; i < 30; i = i + 5) {
+    console.log("Cinco "+ i);
+}
+*/
+/*
+let numero = parseInt(prompt("ingrese un numero"));
+
+for (let i = 0; i <= 10; i++){
+    let multiplicacion = numero * i;
+    let mensaje = `${numero}` * ${i} = ${multiplicacion};
+    
+    alert(mensaje);
+}
+*/
+
+/*
+// codigo para asignar # de lugar a cada usuario
+for (let turno = 1; turno <= 10; turno++){
+    let nombre = prompt("Ingrese su nombre para asignar el turno");
+    let mensaje = `Turno #${turno} Nombre: ${nombre}`;
+    alert(mensaje);
+}
+*/
+
+
+for(let i = 0; i <= 10; i++){
+    if(i === 5){
+        break; // Break rompe el bucle cuando se cumple la condición
+    }
+    console.log(i);
+}
+console.log("Terminamos");
